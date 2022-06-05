fc71b060cd867711a480da6417b232ede6ced8b6
AnaGT <amgiraldot@unal.edu.co>
2022-06-05 14:57:00 -0500

    cargar

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+if __name__ == '__main__':
+    list1 = []
+    list2 = []
+
+    for line in sys.stdin:
+
+        list1.append(line)
+    list2= sorted(list1, key= lambda x: x[2])
+    for item in list2:
+        sys.stdout.write("{}".format(item))
