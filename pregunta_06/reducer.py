a6ecdcccd4f45cb49f506c8ac363933c64ed8a87
AnaGT <86501883+AnaGT@users.noreply.github.com>
2022-06-14 21:16:33 -0500

    Update reducer.py

--- pregunta_06/reducer.py
+++ pregunta_06/reducer.py
@@ -2,38 +2,26 @@
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
 import sys
-
-#
-# Esta funcion reduce los elementos que tienen la misma clave
-#
 if __name__ == '__main__':
+    curkey = None
+    total1 = 0
+    total2 = 0
 
-    lista = []
-    #
-    # cada linea de texto recibida se adiciona a la lista1
-    #
     for line in sys.stdin:
-        line1 = line.replace('\n','').split(',')
-        line1[2] = int(line1[2])
-        lista.append(line1)
 
-        curkey = None
-        total1 = 0
-        total2 = 0
+        key, val = line.split("\t")
+        val = float(val)
 
-        for line in lista:
-
-            key, val = line.split(",")
-            val = float(val)
-
-            if key == curkey:
-                if val > total1:
-                    total1 = val
-
-            else:
-                if curkey is not None:
-                    sys.stdout.write("{},{},{}\n".format(curkey, total1 , total2))
-                curkey = key
-                total1 = val
-                total2 = val
-        sys.stdout.write("{},{},{}\n".format(curkey, total1 , total2))
+        if key == curkey:
+             if val > total1:
+                 total1 = val
+                 if key == curkey:
+                    if val < total1:
+                        total2 = val
+        else:
+            if curkey is not None:
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, total1 , total2))
+            curkey = key
+            total1 = val
+            total2 = val
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, total1 , total2))
